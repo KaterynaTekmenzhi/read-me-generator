f1d74a5f166faac4a756523544a9231a2c6f40df
Kateryna Tekmenzhi <kateryna.koltunova@gmail.com>
2022-05-14 22:52:27 -0700

    updating method of code

--- index.js
+++ index.js
@@ -4,66 +4,76 @@ const fs = require("fs");
 const generateMarkdown= require("./Develop/utils/generateMarkdown");
 
 // Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        message: 'What is the application title?',
-        name: 'title' 
-    },
-    {
-        type: 'input',
-        message: 'What is the application description?',
-        name: 'description'
-    },
-    {
-        type: 'input',
-        message: 'What is the application installation instructions?',
-        name: 'installation'
-    },
-    {
-        type: 'input',
-        message: 'What is the application usage information?',
-        name: 'usage'
-    },
-    {
-        type: 'input',
-        message: 'What is the application license?',
-        name: 'license'
-    },
-    {
-        type: 'input',
-        message: 'Who is the application author?',
-        name: 'author'
-    },
-    {
-        type: 'input',
-        message: 'What is the application contributing guidelines?',
-        name: 'contribution'
-    },
-    {
-        type: 'input',
-        message: 'What is the application tests?',
-        name: 'tests'
-    },
-    {
-        type: 'input',
-        message: 'What is your Github username?',
-        name: 'github'
-    },
-];
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions).then(function(response) {
-        const data = generateMarkdown(response);
-        fs.writeFile('./Develop/utils/README.md', data, function(err) {
-            if (err) {
-                return console.log(err);
-            }
-            console.log('Success!');
-        });
-    });
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the application title?',
+            name: 'title' 
+        },
+        {
+            type: 'input',
+            message: 'What is the application description?',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'What is the application installation instructions?',
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: 'What is the application usage information?',
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: 'What is the application license?',
+            name: 'license'
+        },
+        {
+            type: 'input',
+            message: 'Who is the application author?',
+            name: 'author'
+        },
+        {
+            type: 'input',
+            message: 'What is the application contributing guidelines?',
+            name: 'contribution'
+        },
+        {
+            type: 'input',
+            message: 'What is the application tests?',
+            name: 'tests'
+        },
+        {
+            type: 'input',
+            message: 'What is your Github username?',
+            name: 'github'
+        },
+    ]);
 };
+
+// // TODO: Create a function to initialize app
+// function init() {
+//     inquirer.prompt(questions).then(function(response) {
+//         const data = generateMarkdown(response);
+//         fs.writeFile('./Develop/utils/README.md', data, function(err) {
+//             if (err) {
+//                 return console.log(err);
+//             }
+//             console.log('Success!');
+//         });
+//     });
+// };
+
+const init = () => {
+    questions()
+      // Use writeFileSync method to use promises instead of a callback function
+      .then((answers) => fs.writeFileSync('index.html', generateMarkdown(answers)))
+      .then(() => console.log('Successfully wrote to index.html'))
+      .catch((err) => console.error(err));
+  };
 
 // Function call to initialize app
 init();
