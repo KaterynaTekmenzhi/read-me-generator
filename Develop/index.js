90c8cea9dc9de6f9c995de8734b5c8eee2fb87df
Kateryna Tekmenzhi <kateryna.koltunova@gmail.com>
2022-05-14 22:00:25 -0700

    setting up questions to generate

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown= require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the application title?',
+        name: 'title' 
+    },
+    {
+        type: 'input',
+        message: 'What is the application description?',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What is the application installation instructions?',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'What is the application usage information?',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'What is the application license?',
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'Who is the application author?',
+        name: 'author'
+    },
+    {
+        type: 'input',
+        message: 'What is the application contributing guidelines?',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'What is the application tests?',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'github'
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
